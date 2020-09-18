acd53cf336ec9a28cac009a70cbaba7759520ee4
kensuke10103 <senjukensuke1010@icloud.com>
2020-09-18 15:46:44 +0900

    追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -54,7 +54,7 @@ $('#new_message').on('submit', function(e){
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight}, 50);
       $('form')[0].reset();
       $(".form__submit").prop('disabled', false);
     })

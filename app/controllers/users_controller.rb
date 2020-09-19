0af6249ad16aac3805fff0be72af5d4452cb6496
kensuke10103 <senjukensuke1010@icloud.com>
2020-09-19 15:03:34 +0900

    自動更新機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,27 @@
 class UsersController < ApplicationController
-
-    def edit
+  def index
+    return nil if params[:keyword] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: params[:user_ids]).limit(10)
+    respond_to do |format|
+      format.html
+      format.json
     end
+  end
 
-    def update
-        if current_user.update(user_params)
-          redirect_to root_path
-        else
-          render :edit
-        end
-    end
+  def edit
+  end
 
-    private
+  def update
+      if current_user.update(user_params)
+        redirect_to root_path
+      else
+        render :edit
+      end
+  end
 
-    def user_params
+  private
+
+  def user_params
         params.require(:user).permit(:name, :email)
-    end
+  end
 end
